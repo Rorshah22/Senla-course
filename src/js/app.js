9238527264c6fdfd2ea93a0069ff9b6b2284b935
Siarhei Khalimon <hulk145@gmail.com>
2021-08-16 12:06:19 +0300

    feat: add animation hover

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,11 @@
 import '../scss/app.scss';
+
+const btn = document.querySelector('.menu-burger');
+
+btn.addEventListener('click', () => {
+  btn.classList.toggle('open-menu');
+  console.log('hi');
+  document.querySelector('.social-list').classList.toggle('hidden');
+  // document.querySelector('.menu-box').classList.toggle('open');
+  document.querySelector('.menu-box').classList.toggle('menu-list');
+});
