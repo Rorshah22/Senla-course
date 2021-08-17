44901559c98b9f0e616a200b18b7a4b67259bc83
Siarhei Khalimon <hulk145@gmail.com>
2021-08-17 20:05:13 +0300

    feat: menu close

--- src/js/app.js
+++ src/js/app.js
@@ -3,21 +3,32 @@ import '../scss/app.scss';
 const btn = document.querySelector('.menu-burger');
 const links = document.querySelectorAll('.menu-item');
 const openMenu = document.querySelectorAll('.menu-box');
+const close = document.querySelector('.close');
 
-btn.addEventListener('click', (e) => {
+btn.addEventListener('click', () => {
   btn.classList.toggle('open-menu');
   openMenu.forEach((element) => {
     element.classList.toggle('open');
   });
   document.querySelector('body').classList.toggle('overflow');
+  document.querySelector('.close').classList.toggle('hidden');
 });
 
 links.forEach((link) => {
-  link.addEventListener('click', (e) => {
-    document.querySelector('body').classList.remove('overflow');
-    openMenu.forEach((element) => {
-      element.classList.remove('open');
-    });
-    btn.classList.remove('open-menu');
+  link.addEventListener('click', () => {
+    exitMenu();
   });
 });
+
+close.addEventListener('click', (e) => {
+  exitMenu();
+  // console.log(e.target);
+});
+
+function exitMenu() {
+  document.querySelector('body').classList.remove('overflow');
+  openMenu.forEach((element) => {
+    element.classList.remove('open');
+  });
+  btn.classList.remove('open-menu');
+}
