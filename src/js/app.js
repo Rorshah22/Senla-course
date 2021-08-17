d61cb7c2efbb741ac2842701914ba0a0e125a352
Siarhei Khalimon <hulk145@gmail.com>
2021-08-17 13:22:33 +0300

    fix: header

--- src/js/app.js
+++ src/js/app.js
@@ -4,8 +4,8 @@ const btn = document.querySelector('.menu-burger');
 
 btn.addEventListener('click', () => {
   btn.classList.toggle('open-menu');
-  console.log('hi');
-  document.querySelector('.social-list').classList.toggle('hidden');
-  // document.querySelector('.menu-box').classList.toggle('open');
-  document.querySelector('.menu-box').classList.toggle('menu-list');
+  const openMenu = document.querySelectorAll('.menu-box');
+  openMenu.forEach((element) => {
+    element.classList.toggle('open');
+  });
 });
