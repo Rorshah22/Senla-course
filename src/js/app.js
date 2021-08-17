e19f4a95faeaad285d097daedd5b82c37f29bbc4
Siarhei Khalimon <hulk145@gmail.com>
2021-08-17 19:41:06 +0300

    feat: burger menu

--- src/js/app.js
+++ src/js/app.js
@@ -1,12 +1,23 @@
 import '../scss/app.scss';
 
 const btn = document.querySelector('.menu-burger');
+const links = document.querySelectorAll('.menu-item');
+const openMenu = document.querySelectorAll('.menu-box');
 
-btn.addEventListener('click', () => {
+btn.addEventListener('click', (e) => {
   btn.classList.toggle('open-menu');
-  const openMenu = document.querySelectorAll('.menu-box');
   openMenu.forEach((element) => {
     element.classList.toggle('open');
   });
-  document.querySelector('.header').classList.toggle('header-background-menu');
+  document.querySelector('body').classList.toggle('overflow');
+});
+
+links.forEach((link) => {
+  link.addEventListener('click', (e) => {
+    document.querySelector('body').classList.remove('overflow');
+    openMenu.forEach((element) => {
+      element.classList.remove('open');
+    });
+    btn.classList.remove('open-menu');
+  });
 });
